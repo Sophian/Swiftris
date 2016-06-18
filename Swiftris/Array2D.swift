e3849a8331ebddf96c88a8fdeb29122913f39db9
Sophian <bensaou@gmail.com>
2016-06-18 02:44:25 -0700

    Initial commit

---
+++ Swiftris/Array2D.swift
@@ -0,0 +1,30 @@
+//
+//  Array2D.swift
+//  Swiftris
+//
+//  Created by Sophian Bensaou on 6/18/16.
+//  Copyright © 2016 ___sophian___. All rights reserved.
+//
+
+class Array2D<T> {
+    let columns: Int
+    let rows: Int
+    
+    var array: Array<T?>
+    
+    init(columns: Int, rows: Int) {
+        self.columns = columns
+        self.rows = rows
+        
+        array = Array<T?>(count: rows * columns, repeatedValue: nil)
+    }
+    
+    subscript(column: Int, row: Int) -> T? {
+        get {
+            return array[(row * columns) + column]
+        }
+        set(newValue) {
+            array[(row * columns) + column] = newValue
+        }
+    }
+}
